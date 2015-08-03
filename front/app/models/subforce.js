0399b93a7795b4eb1b66835011e1f91e086124f8
Mbiondo <mbiondo@goblab.org>
2015-08-03 19:47:35 -0300

    Charge

---
+++ front/app/models/subforce.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	name: DS.attr('string'),
+	color: DS.attr('string'),
+	initials: DS.attr('string'),
+	history: DS.attr('string'),
+	logo: DS.belongsTo('asset', {async: true}),
+	force: DS.belongsTo('force', {async: true}),
+});
