b6844fbbb0c93503aefa71d4e590f5b945623384
Maximiliano Biondo <mbiondo@goblab.org>
2015-08-08 17:14:29 -0300

    :D

--- backend/api/controllers/CandivoteController.js
+++ backend/api/controllers/CandivoteController.js
@@ -7,7 +7,7 @@
 
 module.exports = {
 	find:  function findRecords( req, res ) {
-		var query = 'SELECT candivote.id, c.id as candidate, candivote.votes, candivote.board, candivote.borough, candivote.instance, candivote.school, candivote.config FROM candivote  LEFT JOIN candidate c ON candivote.candidate = c.id WHERE c.id > 0 AND candivote.config = ' + parseInt(req.query.config) + ' AND candivote.instance = ' + parseInt(req.query.instance) + ' AND candidate.board = ' + parseInt(req.query.board) + ' ORDER BY CAST(c.order AS SIGNED);';
+		var query = 'SELECT candivote.id, c.id as candidate, candivote.votes, candivote.board, candivote.borough, candivote.instance, candivote.school, candivote.config FROM candivote  LEFT JOIN candidate c ON candivote.candidate = c.id WHERE c.id > 0 AND candivote.config = ' + parseInt(req.query.config) + ' AND candivote.instance = ' + parseInt(req.query.instance) + ' AND candivote.board = ' + parseInt(req.query.board) + ' ORDER BY CAST(c.order AS SIGNED);';
 
 		Candivote.query(query, function (err, results) { 
 			if (!results) {
