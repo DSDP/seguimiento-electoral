a4594ed06b85c8802e3e2011b247cd90b4f18fb2
Mbiondo <mbiondo@goblab.org>
2015-09-01 16:32:18 -0300

    :D

--- backend/api/controllers/CandivoteController.js
+++ backend/api/controllers/CandivoteController.js
@@ -11,16 +11,12 @@ var util = require( 'util' ),
 
 module.exports = {
 	find:  function findRecords( req, res ) {
-		//var query = 'SELECT candivote.id, c.id as candidate, candivote.votes, candivote.board, candivote.borough, candivote.instance, candivote.school, candivote.config FROM candivote  LEFT JOIN candidate c ON candivote.candidate = c.id WHERE c.id > 0 AND candivote.config = ' + parseInt(req.query.config) + ' AND candivote.instance = ' + parseInt(req.query.instance) + ' AND candivote.board = ' + parseInt(req.query.board) + ' ORDER BY CAST(c.order AS SIGNED);';
-
 		var query = Config.findOne( req.query.config ).populate('candidates');
 
 	  	query.exec( function found( err, matchingRecord ) {
 			var candivotes = [];
 
 			Board.findOne(req.query.board).populate('school').exec(function (err, board) {
-				console.log(board.school.borough);
-
 				Borough.findOne(board.school.borough).exec(function (err, borough) {
 					_.each(matchingRecord.candidates, function (candidate) {
 				 		var p = {
@@ -33,7 +29,6 @@ module.exports = {
 				 		};
 				 		candivotes.push(p);
 				 	});
-
 				 	Candivote.findOrCreate(candivotes).exec(function (err, results) {
 						if (!results) {
 							results = [];
