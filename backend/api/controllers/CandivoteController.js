8dc84fc18743d1df43ccb8ed40e120ddbb0db34b
Mbiondo <mbiondo@goblab.org>
2015-09-01 15:59:53 -0300

    Rompi todo

--- backend/api/controllers/CandivoteController.js
+++ backend/api/controllers/CandivoteController.js
@@ -4,17 +4,45 @@
  * @description :: Server-side logic for managing candivotes
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var Q = require('q');
+var util = require( 'util' ),
+  actionUtil = require( '../blueprints/_util/actionUtil' );
+
 
 module.exports = {
 	find:  function findRecords( req, res ) {
-		var query = 'SELECT candivote.id, c.id as candidate, candivote.votes, candivote.board, candivote.borough, candivote.instance, candivote.school, candivote.config FROM candivote  LEFT JOIN candidate c ON candivote.candidate = c.id WHERE c.id > 0 AND candivote.config = ' + parseInt(req.query.config) + ' AND candivote.instance = ' + parseInt(req.query.instance) + ' AND candivote.board = ' + parseInt(req.query.board) + ' ORDER BY CAST(c.order AS SIGNED);';
+		//var query = 'SELECT candivote.id, c.id as candidate, candivote.votes, candivote.board, candivote.borough, candivote.instance, candivote.school, candivote.config FROM candivote  LEFT JOIN candidate c ON candivote.candidate = c.id WHERE c.id > 0 AND candivote.config = ' + parseInt(req.query.config) + ' AND candivote.instance = ' + parseInt(req.query.instance) + ' AND candivote.board = ' + parseInt(req.query.board) + ' ORDER BY CAST(c.order AS SIGNED);';
 
-		Candivote.query(query, function (err, results) { 
-			if (!results) {
-				results = [];
-			}
-			res.ok({candivotes: results});
-		});
+		var query = Config.findOne( req.query.config ).populate('candidates');
+
+	  	query.exec( function found( err, matchingRecord ) {
+			var candivotes = [];
+
+			Board.findOne(req.query.board).populate('school').exec(function (err, board) {
+				console.log(board.school.borough);
+
+				Borough.findOne(board.school.borough).exec(function (err, borough)) {
+					_.each(matchingRecord.candidates, function (candidate) {
+				 		var p = {
+				 			school: board.school.id,
+				 			borough: borough.id,
+				 			candidate: candidate.id,
+				 			instance: req.query.instance,
+				 			board: req.query.board,
+				 			config: req.query.config
+				 		};
+				 		candivotes.push(p);
+				 	});
+
+				 	Candivote.findOrCreate(candivotes).exec(function (err, results) {
+						if (!results) {
+							results = [];
+						}
+						res.ok({candivotes: results});
+				 	});
+				}
+			});
+	  	});
 	}
 };
 
