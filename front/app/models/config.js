df5a9ab68478b59d65d6810f95e29b37eb94fb0c
Mbiondo <mbiondo@goblab.org>
2015-10-22 18:45:13 -0300

    :D

--- front/app/models/config.js
+++ front/app/models/config.js
@@ -10,5 +10,9 @@ export default DS.Model.extend({
 	candidates: DS.hasMany('candidate', {async: true}),
 	instances: DS.hasMany('instance', {async: true}),
 	isVersus: DS.attr('boolean'),
-	order: DS.attr('string')
+	order: DS.attr('string'),
+
+	orden: Ember.computed('order', function () {
+		return parseInt(this.get('order'));
+	})
 });
