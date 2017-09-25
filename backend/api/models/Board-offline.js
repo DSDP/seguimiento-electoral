2eeba2309a06e745a4ece4264b8e71aaf8c49964
Mbiondo <mbiondo@goblab.org>
2017-09-25 04:25:36 -0300

    offline

--- backend/api/models/Board-offline.js
+++ backend/api/models/Board-offline.js
@@ -59,7 +59,7 @@ module.exports = {
 
           _.each(records, function (candivote) {
             var index = _.findIndex(votes, { 'candidateId': candivote.candidate});
-            console.log(candivote.candidate, index);
+            console.log(candivote, index);
             if (index) {
               candivote.votes = votes[index].votes;
               candivote.save();
