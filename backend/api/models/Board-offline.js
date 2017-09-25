5a633e7cb85bdf483453b72e0abba3dae7e82247
Mbiondo <mbiondo@goblab.org>
2017-09-25 04:37:44 -0300

    offline

--- backend/api/models/Board-offline.js
+++ backend/api/models/Board-offline.js
@@ -41,11 +41,11 @@ module.exports = {
     var self = this;
     Board.findOne({name: args.number, town: args.town}).populate('school').exec(function (err, board) {
       console.log(unescape(args.votes.replace('\\', '').replace('"', "'")));
-        var votes = JSON.parse(unescape(args.votes));
+        var votes = JSON.parse(JSON.parse(args.votes));
         var candivotes = [];
 
         _.each(votes, function (candidate) {
-          //console.log(candidate);
+          console.log(candidate);
           var p = {
             school: board.school.id,
             borough: board.school.borough,
