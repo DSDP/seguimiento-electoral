6b518f2f7c5f38870b5597a0bce0a0e4e7225857
Mbiondo <mbiondo@goblab.org>
2017-10-02 20:59:55 -0300

    :D

--- backend/api/models/Board-offline.js
+++ backend/api/models/Board-offline.js
@@ -47,50 +47,53 @@ module.exports = {
     Board.findOne({name: args.number, town: args.town}).populate('school').exec(function (err, board) {
         if (board === undefined) next();
 
-        var votes = JSON.parse(args.votes);
+          if (board) {
 
-        if (!votes[0].candidateId) {
-          votes = JSON.parse(votes);         
-        }
+            var votes = JSON.parse(args.votes);
 
-        var candivotes = [];
-
-        _.each(votes, function (candidate) {
-          var p = {
-            school: board.school.id,
-            borough: board.school.borough,
-            candidate: candidate.candidateId,
-            instance: args.instance,
-            board: board.id,
-            boardOffline: args.id,
-            config: args.config
-          };
-          candivotes.push(p);
-        });
-
-        Candivote.findOrCreate(candivotes).exec(function (err, records) {
-
-          _.each(records, function (candivote) {
-            var index = _.findIndex(votes, { 'candidateId': candivote.candidate});
-            //console.log(candivote, index);
-            if (index >= 0) {
-              candivote.votes = votes[index].votes;
-              candivote.save();
+            if (!votes[0].candidateId) {
+              votes = JSON.parse(votes);         
             }
-          });
+
+            var candivotes = [];
+
+            _.each(votes, function (candidate) {
+              var p = {
+                school: board.school.id,
+                borough: board.school.borough,
+                candidate: candidate.candidateId,
+                instance: args.instance,
+                board: board.id,
+                boardOffline: args.id,
+                config: args.config
+              };
+              candivotes.push(p);
+            });
+
+            Candivote.findOrCreate(candivotes).exec(function (err, records) {
+
+              _.each(records, function (candivote) {
+                var index = _.findIndex(votes, { 'candidateId': candivote.candidate});
+                //console.log(candivote, index);
+                if (index >= 0) {
+                  candivote.votes = votes[index].votes;
+                  candivote.save();
+                }
+              });
 
 
-          board.totalVotes = args.totalVotes;
-          board.blankVotes = args.blankVotes;
-          board.recurredVotes = args.recurredVotes;
-          board.inpugnedVotes = args.inpugnedVotes;
-          board.nullVotes = args.nullVotes;
-          board.isCertificate = args.isCertificate;
-          board.isProvisorio = args.isProvisorio;
+              board.totalVotes = args.totalVotes;
+              board.blankVotes = args.blankVotes;
+              board.recurredVotes = args.recurredVotes;
+              board.inpugnedVotes = args.inpugnedVotes;
+              board.nullVotes = args.nullVotes;
+              board.isCertificate = args.isCertificate;
+              board.isProvisorio = args.isProvisorio;
 
-          board.save();
+              board.save();
 
-          Config.publishUpdate(args.config, {id: args.config}, null, { previous: { } });
+              Config.publishUpdate(args.config, {id: args.config}, null, { previous: { } });
+          }
 
           next();
         });
